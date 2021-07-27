bf970c121cf791134be8f11d6916c54da0946750
axlboy <axelito.ur@gmail.com>
2021-07-27 18:21:22 -0400

    Creación CoffeMilk

---
+++ src/main/java/com/axlboy/coffemilk/CoffemilkApplication.java
@@ -0,0 +1,33 @@
+package com.axlboy.coffemilk;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class CoffemilkApplication extends Application {
+	public static ConfigurableApplicationContext applicationContext;
+	public static Parent rootNode; //Nodo que inicializa la aplicación
+	public static Stage stage;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception { // escanea toda nuestra aplicacion y busca nuestras dependencias
+		applicationContext = SpringApplication.run(CoffemilkApplication.class);
+		FXMLLoader loader = new FXMLLoader(CoffemilkApplication.class.getResource("/index.fxml"));
+		loader.setControllerFactory(applicationContext::getBean); //carga las dependencias
+		Scene scene = new Scene(loader.load(), 800, 1000, false,SceneAntialiasing.BALANCED);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
---
+++ src/test/java/com/axlboy/coffemilk/CoffemilkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axlboy.coffemilk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffemilkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
