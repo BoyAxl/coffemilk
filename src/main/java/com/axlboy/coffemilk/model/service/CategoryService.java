4db69d45411341b5fdd04b4d536bf68543692281
axlboy <axelito.ur@gmail.com>
2021-07-29 18:17:54 -0400

    Interafaz de Productos

--- src/main/java/com/axlboy/coffemilk/model/service/CategoryService.java
+++ src/main/java/com/axlboy/coffemilk/model/service/CategoryService.java
@@ -1,19 +1,30 @@
 package com.axlboy.coffemilk.model.service;
 
+import com.axlboy.coffemilk.model.PostException;
 import com.axlboy.coffemilk.model.entity.Category;
 import com.axlboy.coffemilk.model.repo.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
 @Service
 public class CategoryService {
-    
+
     @Autowired
     private CategoryRepo repo;
 
     public List<Category> findAll(){
         return repo.findAll();
+    }
+
+    public void save(Category category) {
+
+        if (ObjectUtils.isEmpty(category.getName())){
+            throw new PostException("Por favor ingrese un nombre de categoría.");
+        }
+
+        repo.save(category);
     }
 }
--- src/main/java/com/axlboy/coffemilk/views/Categories.java
+++ src/main/java/com/axlboy/coffemilk/views/Categories.java
@@ -2,6 +2,7 @@ package com.axlboy.coffemilk.views;
 
 import com.axlboy.coffemilk.model.entity.Category;
 import com.axlboy.coffemilk.model.service.CategoryService;
+import com.axlboy.coffemilk.views.popups.CategoryEdit;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -9,6 +10,8 @@ import javafx.scene.layout.TilePane;
 import javafx.scene.shape.SVGPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.function.Consumer;
 
 @Controller
 public class Categories extends AbstractController{
@@ -21,23 +24,41 @@ public class Categories extends AbstractController{
 
     @FXML
     private void initialize(){
+        reload();
+    }
+
+    @FXML
+    private void addNew(){
+        CategoryEdit.showView(this::save);
+    }
+
+    private void save(Category category){
+        service.save(category);
+        reload();
+    }
+
+    private void reload(){
         container.getChildren().clear();
-        service.findAll().stream().forEach(category -> container.getChildren().add(new CategoryItem(category)));
+        service.findAll().stream().forEach(category -> container.getChildren().add(new CategoryItem(category, this::save)));
     }
 
     private class CategoryItem extends HBox {
 
-        private SVGPath icon;
-        private Label name;
-
-        public CategoryItem(Category category) {
-            icon = new SVGPath();
+        public CategoryItem(Category category, Consumer<Category> consumer) {
+            SVGPath icon = new SVGPath();
             icon.setContent("M34.001 11.499c0 1.38 1.12 2.5 2.5 2.5s2.5-1.12 2.5-2.5-1.12-2.5-2.5-2.5-2.5 1.12-2.5 2.5zM16.521 46.839c1.54 1.551 4.040 1.551 5.59 0l24.44-24.28c0.979-0.979 1.47-2.27 1.45-3.56v-14.040c0-2.74-2.221-4.96-4.96-4.96h-14.040c-1.29-0.021-2.58 0.47-3.561 1.45l-24.279 24.441c-1.551 1.549-1.551 4.049 0 5.59l15.36 15.359zM30.001 11.499c0-3.59 2.91-6.5 6.5-6.5s6.5 2.91 6.5 6.5-2.91 6.5-6.5 6.5-6.5-2.91-6.5-6.5z");
-            name = new Label();
+            Label name = new Label();
             name.setText(category.getName()); // le pasamos el atributo name al nombre del label
 
             getChildren().addAll(icon, name);
             getStyleClass().add("category-item"); //Le seteamnos un estilo del css
+
+            setOnMouseClicked(event -> {
+
+                if(event.getClickCount() == 1 ){
+                    CategoryEdit.showView(category, consumer);
+                }
+            });
         }
     }
 }
